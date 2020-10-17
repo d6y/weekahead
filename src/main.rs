67f1200c89cb8f556d00a6450a6bf801f864cced
Richard Dallaway <richard@dallaway.com>
2020-10-17 17:14:12 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use chrono::prelude::*;
+use std::io::Error;
+use std::io::Write;
+use std::fs::File;
+
+fn main() -> Result<(), Error> {
+    let first_day = monday(Utc::today());
+    let days: Vec<Date<Utc>> = week(first_day);
+
+    let title = first_day.format("Week %d %b %Y");
+
+    let mut file = File::create(format!("{}.md", title))?;
+    write!(&file, "# {}\n", title)?;
+
+    for day in days {
+        layout(&mut file, &day)?;
+    }
+
+    Ok( () )
+}
+
+fn layout(file: &mut File, day: &Date<Utc>) -> Result<(), Error> {
+    let day_heading = day.format("%d %b %Y (%a)");
+    let tags = day.format("#Journal/OnThisDay/%m/%d# #Journal/%Y/%m#");
+    write!(file, "## {}\n\n\n{}\n\n---\n\n", day_heading, tags)
+}
+
+fn monday(from: Date<Utc>) -> Date<Utc> {
+    if from.weekday() == Weekday::Mon {
+        from
+    } else {
+        monday(from.succ())
+    }
+}
+
+fn week(from: Date<Utc>) -> Vec<Date<Utc>> {
+    let mut cursor = from.clone();
+    let mut days = Vec::with_capacity(7);
+
+    (1..=7).for_each(|_| {
+        days.push(cursor);
+        cursor = cursor.succ();
+    });
+
+    days
+}
